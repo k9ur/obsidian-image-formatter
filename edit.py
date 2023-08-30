87b64250f866ac9728b585c351ea8a5fbd8a1d9f
k9wolf8634 <k9wolf@pm.me>
2023-08-30 12:13:21 +0800

    Allow for editing of saved images

--- edit.py
+++ edit.py
@@ -5,11 +5,16 @@ HUE_SHIFT_DEG = MAX // 2
 BRIGHTNESS_INCR = 30
 
 
+from_file = False
+
 # Get image
 png_image = ImageGrab.grabclipboard()
 if png_image is None:
 	input("No image in clipboard. Press enter to exit... ")
 	exit(1)
+elif isinstance(png_image, list):
+	from_file = True
+	png_image = Image.open(png_image[0])
 
 
 # Ask whether the image should be hue-shifted
@@ -18,8 +23,16 @@ response = input("Also shift hue? (y/n) ").strip().lower()
 hue_shift = response == "y"
 
 
+png_image.convert("RGBA") # Not sure if this is useful in any cases
+
 # Preserve alpha, recombined later
-r, g, b, a = png_image.split()
+png_image_split = png_image.split()
+a = png_image_split[3] if len(png_image_split) == 4 else None
+if a is None: # Generate alpha
+	alpha_opaque = Image.new("RGBA", png_image.size, "black")
+	a = alpha_opaque.split()[3]
+r, g, b = png_image_split[:3]
+
 rgb_image = Image.merge("RGB", (r, g, b))
 
 inverted_image = ImageOps.invert(rgb_image)
@@ -49,3 +62,7 @@ final_png_image = Image.merge("RGBA", \
 # Display image
 # Doesn't copy to clipboard
 final_png_image.show()
+
+# Close image if opened
+if from_file:
+	png_image.close()
